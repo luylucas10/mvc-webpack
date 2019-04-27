d7162b0e312b80f3253ad0b8a096635abe18d97b
Luy Lucas <luylucas10@hotmail.com>
2019-04-27 18:42:36 -0400

    Part 6

---
+++ MvcWebpackVue/public/src/js/home/index.js
@@ -0,0 +1,10 @@
+﻿import Vue from "vue";
+
+const vm = new Vue({
+    el: "#app-home",
+    data() {
+        return {
+            message: "Hello World with .NET MVC and Vue.js!!!"
+        };
+    }
+});
